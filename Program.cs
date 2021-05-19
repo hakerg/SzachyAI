9763c61f41988ccd952c94127726ae69f5fd65f5
hakerg <grzegorzabedzki@gmail.com>
2021-05-19 19:46:49 +0200

    update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,18 +12,9 @@ namespace SzachyAI {
         private static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< HEAD
             try {
-                Application.Run(new Form1());
-            } catch (Exception e) {
-=======
-            try
-            {
                 Application.Run(new MenuForm());
-            }
-            catch (Exception e)
-            {
->>>>>>> 3c94f40f9719ddbcbddb8abf480f54a7267aecfe
+            } catch (Exception e) {
                 MessageBox.Show(e.Message);
             }
         }
